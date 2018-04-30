7e3cd282deb5eed250dd46aee2b4162409498c0c
Mrinaank SInha <car.knives@gmail.com>
2018-04-30 21:18:37 +1000

    Added opencv library to core project to remove dependece on OpenCVManager and reduced APK file size (94.71 --> 18.45 :O)

--- app/src/main/java/com/mrinaanksinha/majorworkandroid/MainActivity.java
+++ app/src/main/java/com/mrinaanksinha/majorworkandroid/MainActivity.java
@@ -1,15 +1,64 @@
 package com.mrinaanksinha.majorworkandroid;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-import org.opencv.android.*;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Mat img;
+    Bitmap bitmap;
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case LoaderCallbackInterface.SUCCESS:{
+                    img = new Mat();
+
+                }
+                break;
+                default:{
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 1;
+        bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher,options);
+        if(!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for Initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+        }else{
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+
+
+        Utils.bitmapToMat(bitmap,img);
+
+        Imgproc.cvtColor(img,img,Imgproc.COLOR_BGR2GRAY);
+        Utils.matToBitmap(img,bitmap);
+
+
+
+
+
+        //fd
+
     }
 }
