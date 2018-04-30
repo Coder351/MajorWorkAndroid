6189921b84b17945e0801838d4656698575d4a27
Mrinaank SInha <car.knives@gmail.com>
2018-04-30 19:05:35 +1000

    Test

--- app/src/main/java/com/mrinaanksinha/majorworkandroid/MainActivity.java
+++ app/src/main/java/com/mrinaanksinha/majorworkandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.mrinaanksinha.majorworkandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.opencv.android.*;
 
 public class MainActivity extends AppCompatActivity {
 
